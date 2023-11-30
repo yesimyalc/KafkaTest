990232072b3bad9eb3ce208c5fa43af5cd743570
Yeşim Yalçın <80751723+yesimyalc@users.noreply.github.com>
2023-11-30 12:50:40 +0300

    .

--- src/main/java/com/jade/KafkaTest/consumer/KafkaConsumer.java
+++ src/main/java/com/jade/KafkaTest/consumer/KafkaConsumer.java
@@ -1,15 +1,18 @@
 package com.jade.KafkaTest.consumer;
 
 import com.jade.KafkaTest.util.AppConstants;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 public class KafkaConsumer {
 
     @KafkaListener(topics= AppConstants.TOPIC_NAME, groupId = AppConstants.GROUP_ID)
     public void consume(String message) throws InterruptedException {
-        System.out.println("Kafka Consumer: "+message);
+        log.log(Level.INFO, "Consumer: "+message);
         Thread.sleep(10L);
     }
 }
--- src/main/java/com/jade/KafkaTest/producer/KafkaProducer.java
+++ src/main/java/com/jade/KafkaTest/producer/KafkaProducer.java
@@ -1,18 +1,22 @@
 package com.jade.KafkaTest.producer;
 
 import com.jade.KafkaTest.util.AppConstants;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 public class KafkaProducer {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaProducerTemplate;
 
     public void sendMessage(String message){
-        System.out.println("Kafka Producer: "+message);
+
+        log.log(Level.INFO, "Producer: "+message);
         kafkaProducerTemplate.send(AppConstants.TOPIC_NAME, message);
     }
 }
